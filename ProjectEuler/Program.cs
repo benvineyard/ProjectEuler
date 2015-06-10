646fd4bb5900fa41568a4bf6e2215243fc88e24e
benvineyard <ben.vineyard@gmail.com>
2015-06-09 17:24:28 -0700

    Added problem #3, calculation of largest prime factor for 600851475143.

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -12,6 +12,7 @@ namespace ProjectEuler
         {  
             Console.WriteLine(string.Format("Problem #1: {0}", Problem1.Answer()));
             Console.WriteLine(string.Format("Problem #2: {0}", Problem2.EvenFibonacciNumbersSum()));
+            Console.WriteLine(string.Format("Problem #2: {0}", Problem3.GetLargestPrimeFactor()));
 
             Console.ReadKey();
         }
