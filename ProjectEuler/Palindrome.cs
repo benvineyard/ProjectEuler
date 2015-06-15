80698201d8933adc03fdf093bd5412e8db3f9bcb
benvineyard <ben.vineyard@gmail.com>
2015-06-15 13:20:47 -0700

    1) Added basic text menu to select a question number and have the answer presented instead of spitting out all of the answers. 2) Added problem #4: "Find the largest palindrome made from the product of two 3-digit numbers."

---
+++ ProjectEuler/Palindrome.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Palindrome
+    {
+        /// <summary>
+        /// Calculates the largest possible palindrome from the product of 
+        /// two three digit numbers.
+        /// </summary>
+        /// <returns>The largest possible palindrome.</returns>
+        internal static int GetLargestFromProductOfTwoThreeDigitNumbers()
+        {
+            int largestProduct = 0;
+
+            for (int x = 0; x < 1000; x++)
+            {
+                for (int y = 0; y < 1000; y++)
+                {
+                    int nextProduct = x * y;
+
+                    if (nextProduct > largestProduct && IsPalindrome(nextProduct.ToString()))
+                        largestProduct = nextProduct;
+                }
+            }
+
+            return largestProduct;
+        }
+
+        /// <summary>
+        /// Determines whether or not a provided string is a palindrome or not.
+        /// </summary>
+        /// <param name="stringToCheck">The provided string to check for palindromic conditions.</param>
+        /// <returns>True if the provided string is a palindrome, otherwise false.</returns>
+        private static bool IsPalindrome(string stringToCheck)
+        {
+            if (string.IsNullOrEmpty(stringToCheck))
+                throw new ArgumentException("String cannot be null.");
+
+            string reversed = new string(stringToCheck.ToCharArray().Reverse().ToArray());
+
+            if (stringToCheck == reversed)
+                return true;
+
+            return false;
+        }
+    }
+}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -9,11 +9,19 @@ namespace ProjectEuler
     class Program
     {
         static void Main(string[] args)
-        {  
-            Console.WriteLine(string.Format("Problem #1: {0}", Problem1.Answer()));
-            Console.WriteLine(string.Format("Problem #2: {0}", Problem2.EvenFibonacciNumbersSum()));
-            Console.WriteLine(string.Format("Problem #2: {0}", Problem3.GetLargestPrimeFactor()));
+        {
+            bool isValidQuestion = false;
+            short questionNumber = 0;
 
+            while (!isValidQuestion)
+            {
+                Console.WriteLine("...:::Please enter the number of the ProjectEuler.net question you would like to see the answer for (1-510):  ");    
+                isValidQuestion = short.TryParse(Console.ReadLine(), out questionNumber);
+
+                if (isValidQuestion)
+                    MenuRouter.Route(questionNumber);
+            }
+            
             Console.ReadKey();
         }
     }
